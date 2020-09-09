2ff0efcfb50665b9dd779a4414cc30a5f7e37963
Mac <xuromeo@gmail.com>
2020-09-08 19:41:40 -0700

    router crud operation

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
+//IMPORT
 const express = require("express");
 const app = express();
-const port = process.env.port || 4444;
-app.use(express.json());
-app.get("/",(req,res)=>{
- res.send('hello express from Romeo')
-})
+const port = process.env.PORT|| 4444;
 const postsRoutes = require('./routes/posts');
+app.use(express.json());
+
+app.get("/",(req,res)=>{
+ res.send('hello express from Romeo for Node api 2')
+})
+
 app.use('/api/posts',postsRoutes);
+
+// function logger(req, res, next) {
+//     console.log("logger function",req);
+//     next();
+//   }
+// app.use(logger());
 
 app.listen(port,err=>{
     if(err){
@@ -14,3 +23,4 @@ app.listen(port,err=>{
     }
     console.log(`listening on ${port}`)
 })
+
--- routes/posts.js
+++ routes/posts.js
@@ -14,6 +14,48 @@ router
   }
 })
 
+router.get('/:id',async (req,res)=>{
+  try{
+    const post = await Posts.findById(req.params.id);
+    if (post.id = req.params.id){
+      res.status(200).json(post)
+    }else{
+      res.status(404).json({message:"could not find it by Id"})
+    }
+  }catch{
+    res.status(500).json({ message: 'The post not found in server' })
+  }
+
+})
+
+router.get('/:id/comments',async (req,res)=>{
+  try{
+    const postComments = await Posts.findPostComments(req.params.id);
+    if (postComments.id = req.params.id){
+      res.status(200).json(postComments)
+    }else{
+      res.status(404).json({message:"could not find comments by Id"})
+    }
+  }catch{
+    res.status(500).json({ message: 'The post not found in server' })
+  }
+
+})
+
+router.delete('/:id',async (req,res)=>{
+  try{
+    const DeletePost = await Posts.remove(req.params.id);
+    if (DeletePost.id = req.params.id){
+      res.status(200).json(DeletePost)
+    }else{
+      res.status(404).json({message:"could not find it by Id"})
+    }
+  }catch{
+    res.status(500).json({ message: 'The post not found in server' })
+  }
+
+})
+
 router.post(`/`, async (req, res) => {
     const post = req.body;
     try {
@@ -28,4 +70,6 @@ router.post(`/`, async (req, res) => {
       res.status(500).json({ message: 'There was an error while saving the post to the database' })
     }
   })
+
+
 module.exports = router;
